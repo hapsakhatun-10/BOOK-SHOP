6f4896b7f036a314845ad76779317469d72a12f8
hapsakhatun-10 <hk.hapsakhatun@gmail.com>
2026-05-02 03:16:33 +0600

    auth

--- src/proxy.js
+++ src/proxy.js
@@ -1,12 +1,16 @@
 import { NextResponse } from "next/server";
-import { auth } from "./lib/auth";
+import { auth } from "@/lib/auth";
 
-export default async function proxy(request) {
+export async function proxy(request) {
     const session = await auth.api.getSession({
         headers: request.headers,
     });
 
-    if (!session) {
+    const isProtected =
+        request.nextUrl.pathname.startsWith("/profile") ||
+        request.nextUrl.pathname.startsWith("/all-books");
+
+    if (isProtected && !session) {
         return NextResponse.redirect(new URL("/signin", request.url));
     }
 
@@ -14,5 +18,5 @@ export default async function proxy(request) {
 }
 
 export const config = {
-    matcher: ["/profile", "/all-books/:path*"],
+    matcher: ["/profile/:path*", "/all-books/:path*"],
 };
